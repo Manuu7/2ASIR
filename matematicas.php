6eb70d512dfb8079be8bbba6162babaff93ae1cb
Manuel Jesus <manusfdk7@gmail.com>
2021-10-15 12:44:37 +0200

    commit 15 octubre

--- matematicas.php
+++ matematicas.php
@@ -7,75 +7,105 @@
     <title>Matemáticas</title>
 </head>
 <body>
-    <?php
-    
-    echo "Ejemplo de bucle while" . "<br>";
 
-    $numero=0;
-    while ($numero <= 10) {
-        #Paso 1:Mostrar mi numero
-        echo "Mi número es: ".$numero."<br>";
-        #$numero++;
-        $numero=$numero+1;
-        
-    }
-    echo "Cuenta terminada"."<br>";
+<?php
 
-    #cuenta de 5 en 5
-$numero1=0; 
-while ($numero1 <= 100) {
-    echo "Cuenta de 5 en 5:  ".$numero1."<br>";
-    $numero1=$numero1+5;
-}
-echo "cuenta terminada" ."<br>";
+echo "Ejemplo de bucle while"."<br>";
 
-#cuenta aleatoria
-$numero2=0;
-$aleatorio=rand(0,100);
-while ($numero2 <= $aleatorio) {
-    echo "El numero va a ser: ". $numero2."<br>";
-    $numero2++;
-}
-echo "cuenta terminada"."<br>";
+$numero=0;
 
-
-
-for ($cuenta=0; $cuenta <= 10; $cuenta++) { 
-    echo "La cuenta es: ".$cuenta."<br>";   
+while ($numero <= 10) {
+    # Paso 1: Mostrar mi número
+    echo "Mi número es: ".$numero."<br>";
+    #Paso 2: Actualizar la variable número
+    #$numero++;  = #$numero=$numero+1;
+    $numero++;
+  
 }
 
+echo "Ejemplo de bucle while"."<br>";
 
+$numero1=0;
+$aleatorio = rand(0,100);
+
+while ($numero1 <= $aleatorio) {
+    # Paso 1: Mostrar mi número
+    echo "Mi número aleatorio es: ".$aleatorio."<br>";
+    echo "Mi número es: ".$numero1."<br>";
+    #Paso 2: Actualizar la variable número
+    #$numero++;  = #$numero=$numero+1;
+    #$numero1=$numero1+5;
+    #$numero++
+    $numero1++;
+  
+}
+
+echo "Mi cuenta ha terminado. "."<br>";
+
+
+echo "<br>"."Ejemplo for "."<br>";
+
+for ($cuenta=0; $cuenta < 10; $cuenta++) { 
+    # Acción
+    print "Imprime la cuenta: ".$cuenta."<br>";
+}
+
+
+$nombre="Jose Angel";
 function persona($nombre,$edad){
 
-    echo "Su nombre es: ".$nombre;
+    echo "Su nombre es : ".$nombre;
+
     if ($edad%2==0) {
-        echo "su edad es par"."<br>";
-    }else{
-        echo "su edad es impar"."<br>";
+
+        echo " y su edad es par"."<br>";
+
+        # code...
+    } else{
+
+        echo "y su edad es impar"."<br>";
     }
 }
-#Funcion presente
+#Función presentate
+function presenta($nombre,$apellidos,$localidad){
 
-function psesentea($nombre,$apellidos,$localidad){
-
-    echo "mi nombre es ".$nombre." ".$apellidos. "y soy de" $localidad."<br>";
+    print "Mi nombre es ".$nombre." ".$apellidos. " y soy de ".$localidad."<br>";
 }
+
+
 
 function suma($num1,$num2){
 
-    $res= $sum1 + $sum2;
-    print "El resultado es ".$res."<br>"
+    $res= $num1 + $num2;
+
+    print "El resultado es ".$res."<br>";
 }
-persona("Miguel ",25);
-persona("Manule ",24);
 
-echo "<br>"."<br>"."<br>"."<br>";
+persona("Miguel",25);
+persona("Manuel",23);
+persona("Adán",22);
+persona("Manule",257);
 
-$alumnos = array("Nacho","Gonzalo","Javi","Adam","Manule","Miguel","JoseAngel","Ivan");
-echo "La cantidad de alumnos es: ".count($alumnos)."<br>"; 
-$selectaleatorio = rand(0,7);
-echo "El alumno es: ".$alumnos[$selectaleatorio];
+presenta("Miguel","Bocachancla","Lora de Estepa");
+$prueba1=5;
+$prueba2=3;
+suma(3,2);
+suma(3,3);
 
-    ?>
+suma($prueba1,$prueba2);
+
+echo"<br>"."<br>"."<br>";
+
+$alumnos = array("Nacho","Gonzalo","Javi","Adán","Manuel","Iván");
+
+echo count($alumnos)."<br>";
+
+$selectAleatorio = rand(0,5);
+
+echo "El alumno es: ".$alumnos[$selectAleatorio]."<br>";
+
+
+
+?>
 </body>
 </html>
