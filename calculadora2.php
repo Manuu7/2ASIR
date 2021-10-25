0a937c1830fb224c3aff057f7f8951b60f50c739
Manuel Jesus <manusfdk7@gmail.com>
2021-10-25 10:39:29 +0200

    commit 25octubre

--- calculadora2.php
+++ calculadora2.php
@@ -1,54 +1,119 @@
 <?php
 
 #Variables metodo POST
-$num1=$_POST["operando1"]
-$num2=$_POST["operando2"]
-$selector=$POST["operador"]
+$numrecog1=$_POST["operando1"];
+$numrecog2=$_POST["operando2"];
+$selector=$_POST["operador"];
 
-echo $selector;
-echo $num1;
-echo $num2;
+
+
 #Funcion Suma
-function suma($num1,$num2){
+function suma($numrecog1,$numrecog2){
 
-    $res= $num1 + $num2;
+    $res= $numrecog1 + $numrecog2;
 
     print "El resultado es ".$res."<br>";
 }
 
-#Funcion Resta
-function resta($num1,$num2){
 
-    $res= $num1 - $num2;
+
+#Funcion Resta
+function resta($numrecog1,$numrecog2){
+
+    $res= $numrecog1 - $numrecog2;
 
     print "El resultado es ".$res."<br>";
 }
 
 #Funcion multiplicacion
-function multiplicacion($num1,$num2){
+function multiplicacion($numrecog1,$numrecog2){
 
-     $res= $num1 * $num2;
+     $res= $numrecog1 * $numrecog2;
 
   print "El resultado es ".$res."<br>";
 }
 
 #Funcion Division
-function division($num1,$num2){
+function division($numrecog1,$numrecog2){
 
-    $res= $num1 / $num2;
+    $res= $numrecog1 / $numrecog2;
 
     print "El resultado es ".$res."<br>";
 }
 
+#Funcion Raiz cuadrada
+function raizcuadrada($numrecog1){
+   
+    $res= sqrt($numrecog1);
+   
+    print "El resultado es ".$res."<br>";
+   
+   }
+
+   #Funcion Elevar al cuadrado
+function elevaralcuadrado($numrecog1){
+   
+    $res=pow($numrecog1,2);
+   
+    print "El resultado es ".$res."<br>";
+   
+   }
+
+   #Funcion Elevar al cubo
+   function elevaralcubo($numrecog1){
+   
+    $res=pow($numrecog1,3);
+   
+    print "El resultado es ".$res."<br>";
+   
+   }
+
+   #Funcion Elevar al cualquier exponente
+   function elevar($numrecog1,$numrecog2){
+   
+    $res=pow($numrecog1,$numrecog2);
+   
+    print "El resultado es ".$res."<br>";
+   
+   }
+
 #Realizacion de variables
 switch ($selector) {
-    case '+': suma($num1,$num2)
-        # code...
-        break;
+    case 'suma': 
+        suma($numrecog1,$numrecog2);
+    break;
     
-    default:
-        # code...
-        break;
+    case 'resta': 
+        resta($numrecog1,$numrecog2);
+    break;
+
+    case 'multiplicaion': 
+        multiplicacion($numrecog1,$numrecog2);
+    break;
+
+    case 'division': 
+        division($numrecog1,$numrecog2);
+    break;
+
+    case 'raiz cuadrada': 
+        raizcuadrada($numrecog1);
+        
+    break;
+
+    case 'elevar cuadrado':
+         elevaralcuadrado($numrecog1,2);
+        
+    break;
+
+    case 'elevar cubo': 
+        elevaralcubo($numrecog1);
+        
+    break;
+
+    case 'elevar': 
+        elevar($numrecog1,$numrecog2);
+        
+    break;
 }
 
 
