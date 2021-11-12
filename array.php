a10d02b47b922aff2086b03e068136e83f86de59
Manuel Jesus <manusfdk7@gmail.com>
2021-11-12 12:39:27 +0100

    commit 12 Nov

--- array.php
+++ array.php
@@ -45,19 +45,97 @@ while ($parada < 5) {
 }
 
 
-#Array Asociativos.
+#ARRAYS ASOCIATIVOS
 
-$alumno = array("Nombre" => "Migue",
-                "Apellidos" => "Kerry",
-                "Edad" => 23,
-                "estaAprobado" => false);
+print "<br><h3>ARRAY ASOCIATIVOS</h3>"."<br>";
 
-echo "El alumno es: " .$alumno["Nombre"].$alumno["Apellidos"].$alumno["Edad"];
+$potencias2=[ 1=>2 , 2=>4 , 3=>8];
+print_r ($potencias2);
+echo "<br>";
+$capitales= array("Andalucia"=> "Sevilla",
+ "Madrid" => "Madrid",
+ "Aragon"=> "Zaragoza",
+ "Baleares"=> "Palma de Mallorca");
+
+ print_r ($potencias);
+ echo "<br>";
+ print_r ($capitales);
+echo "<br>";
+
+#Añadir elementos a un array
+$capitales["Andalucia"]="Almeria ";
+
+print_r ($capitales);
+echo "<br>";
+
+
+$cuentaLetras=strlen($capitales["Andalucia"]);
+echo $cuentaLetras;
+
+
+$notasAsignaturas=["LMI"=>0,
+ "FOL"=>10,
+ "PAR"=>9,
+ "ISO"=>0,
+ "FH"=>0,
+ "BD"=>4];
+ 
+ print_r($notasAsignaturasMigue);
+
+#Unión de Arrays
+
+echo "<br>";
+
+$coches1= array("Seat","Renault","Dacia");
+$coches2= ["BMW","Audi"];
+
+
+print_r($coches1);
+echo "<br>";
+print_r($coches2);
+echo "<br> Unión de arrays <br>";
+
+
+$union1con2=$coches1+$coches2;
+print_r($union1con2);
+echo ">br>";
+
+
+$union1con2=$coches1+$coches2;
+print_r($union1con2);
+echo ">br>";
+
+#Borra elemento
+
+$coches1= array("Seat","Renault","Dacia","BMW","Audi");
+print_r();
+echo "<br>";
+unset ($coches3[1]);
+
+print_r ($coches3);
+echo "<br>";
+$cuentaCoches=count($coches3);
+echo $cuentaCoches;
+
+print $coches3[]="Ferrari";
+print_r($coches3);
+echo "<br>";
 
 
 
+#Copiar matrices
 
+$copiacapitales = $capitales;
+$copiacapitales["Andalucia"]="Cordoba";
+print_r($copiacapitales);
+echo "<br>";
 
+$colores1=["Verde","Azul","Amarillo","Rojo"];
+$colores2=["Verde","Azul","Amarillo","Rojo","Blanco"];
+echo "<br>";
+
+$resultadoDiferencia= array_diff($colores2,$colores1);
+print_r($resultadoDiferencia);
 
 
 
